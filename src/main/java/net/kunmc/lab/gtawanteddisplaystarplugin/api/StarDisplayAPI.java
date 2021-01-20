fb5c245b65475df02d52bb313d642b1c63198a87
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-01-21 00:01:18 +0900

    add てん

--- src/main/java/net/kunmc/lab/gtawanteddisplaystarplugin/api/StarDisplayAPI.java
+++ src/main/java/net/kunmc/lab/gtawanteddisplaystarplugin/api/StarDisplayAPI.java
@@ -6,18 +6,18 @@ public interface StarDisplayAPI
 {
     /**
      * プレイヤに星を表示させる。
-     * @param player 対象プレイヤ
-     * @param now 変更する星の数
-     * @param max 最大星の数
-     * @param blinking 点滅させるかどうか
+     * @param player 対象プレイヤ。
+     * @param now 変更する星の数。
+     * @param max 最大星の数。
+     * @param blinking 点滅させるかどうか。
      */
     void showStar(Player player, int now, int max, boolean blinking);
 
     /**
      * オンラインのすべてのプレイヤに星を表示させる。
-     * @param now 変更する星の数
-     * @param max 最大星の数
-     * @param blinking 点滅させるかどうか
+     * @param now 変更する星の数。
+     * @param max 最大星の数。
+     * @param blinking 点滅させるかどうか。
      */
     void showStarBroadcast(int now, int max, boolean blinking);
 }
