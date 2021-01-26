a7dc80abbfa739cfa2f9d76fcc28e897abbed911
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-01-27 02:25:27 +0900

    警告追加

--- src/main/java/net/kunmc/lab/gtawanteddisplaystarplugin/commands/SetWanted.java
+++ src/main/java/net/kunmc/lab/gtawanteddisplaystarplugin/commands/SetWanted.java
@@ -9,11 +9,18 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class SetWanted implements CommandExecutor
+
 {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage(ChatColor.RED + "プレイヤーから実行してください！");
+            return true;
+        }
+
+        if (!sender.hasPermission("star.set"))
+        {
+            sender.sendMessage(ChatColor.RED + "エラー：権限がありません！");
             return true;
         }
 
